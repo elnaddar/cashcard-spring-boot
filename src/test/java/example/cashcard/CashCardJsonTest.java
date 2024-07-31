15b332b4d88343cc943b7d8e85f5a26fb91de154
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-31 22:42:07 +0300

    my first test.

---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,10 @@
+package example.cashcard;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CashCardJsonTest {
+    @Test
+    void myFirstTest(){
+        assertThat(43).isEqualTo(43);
+    }    
+}
