91368499c7cc3470d5d282fa33a4f2818f4a8342
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-31 23:03:17 +0300

    created the fail test

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,10 +1,26 @@
 package example.cashcard;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@JsonTest
 class CashCardJsonTest {
+    @Autowired
+    private JacksonTester<CashCard> json;
+
     @Test
-    void myFirstTest(){
-        assertThat(43).isEqualTo(43);
+    void cashCardSerializationTest() throws IOException{
+        CashCard cashcard = new CashCard(99L, 123.45);
+        assertThat(json.write(cashcard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashcard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashcard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        assertThat(json.write(cashcard)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashcard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
     }    
 }
