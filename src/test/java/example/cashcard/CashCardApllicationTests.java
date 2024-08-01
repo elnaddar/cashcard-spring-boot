ed071a42fb93bdcdbf2b48901501318b02913d30
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-01 11:03:08 +0300

    [TEST] checking existance of id

--- src/test/java/example/cashcard/CashCardApllicationTests.java
+++ src/test/java/example/cashcard/CashCardApllicationTests.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,5 +20,10 @@ class CashCardApllicationTests {
     void shouldReturnACashCardWhenDataIsSaved(){
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(99);
+        
     }
 }
