ce842c6127245094b86ff548be2d3c4d6cf4ae6d
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-01 23:48:17 +0300

    CashCardController now gets data from data base.

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -17,9 +18,9 @@ class CashCardController {
 
     @GetMapping("/{requestId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
-        if (requestId == 99L) {
-            CashCard cashCard = new CashCard(99L, 0.0);
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCardOptional = cardRepository.findById(requestId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
