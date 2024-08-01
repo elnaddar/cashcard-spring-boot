0a8c282bf9203d8b8e78113818fdfac784ad8bf6
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-01 09:54:07 +0300

    created fail test for get end point

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,16 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+class CashCardController {
+    @GetMapping("/{requestId}")
+    private ResponseEntity<String> findById(){
+        return ResponseEntity.ok("{}");
+    } 
+}
