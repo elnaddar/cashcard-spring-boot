996a7f8f58438a5b8038311b541cee376573894b
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-01 20:46:30 +0300

    [FIX] checking non existing cash card

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cashcards")
 class CashCardController {
     @GetMapping("/{requestId}")
-    private ResponseEntity<CashCard> findById(){
-        CashCard cashCard = new CashCard(99L, 0.0);
-        return ResponseEntity.ok(cashCard);
-    } 
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
+        if (requestId == 99L) {
+            CashCard cashCard = new CashCard(99L, 0.0);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
