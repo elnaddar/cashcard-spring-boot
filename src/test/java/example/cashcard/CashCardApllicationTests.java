c98489f912a78593c22f2351b4b1fe1d170bf0fb
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-01 11:22:30 +0300

    [TEST] checking non existing cash card.

--- src/test/java/example/cashcard/CashCardApllicationTests.java
+++ src/test/java/example/cashcard/CashCardApllicationTests.java
@@ -29,4 +29,11 @@ class CashCardApllicationTests {
         Number amount = documentContext.read("$.amount");
         assertThat(amount).isEqualTo(0.0);
     }
+
+    @Test
+    void shouldNotReturnACashCardWithAnUnknownId(){
+        ResponseEntity<String> respose = restTemplate.getForEntity("/cashcards/1000", String.class);
+        assertThat(respose.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(respose.getBody()).isBlank();
+    }
 }
