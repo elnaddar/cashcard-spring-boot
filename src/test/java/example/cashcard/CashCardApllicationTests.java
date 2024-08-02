4fdda4149e9be2ae9f69c715f5ac3eca4084f06f
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-02 09:52:33 +0300

    [TEST] Updated test case for POST request to ensure 201 response and valid GET URI

--- src/test/java/example/cashcard/CashCardApllicationTests.java
+++ src/test/java/example/cashcard/CashCardApllicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,6 +43,11 @@ class CashCardApllicationTests {
         CashCard newCashCard = new CashCard(null, 250.00);
         ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
 
-        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
