f0732cfba08457e058b23cbc8abeece5d2a52641
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-02 20:16:03 +0300

    [FIX] added default search parameters

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -3,6 +3,7 @@ package example.cashcard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,7 +46,11 @@ class CashCardController {
     @GetMapping
     private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
         Page<CashCard> page = cardRepository.findAll(
-                PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort()));
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
         return ResponseEntity.ok(page.getContent());
     }
 }
