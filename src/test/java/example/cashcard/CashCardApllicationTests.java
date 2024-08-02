1f27009bac1b71deff5d90a8379aae87107be0d5
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-02 19:09:02 +0300

    [FIX] added DirtiesContext to tests to refresh db after each test method.

--- src/test/java/example/cashcard/CashCardApllicationTests.java
+++ src/test/java/example/cashcard/CashCardApllicationTests.java
@@ -11,11 +11,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+
 import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApllicationTests {
     @Autowired
     TestRestTemplate restTemplate;
