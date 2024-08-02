70c8ccf21f169a95033c8d62f74ec945112aca7d
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-02 18:50:11 +0300

    [TEST] wrote fail test for get list of cards.

--- src/test/java/example/cashcard/CashCardApllicationTests.java
+++ src/test/java/example/cashcard/CashCardApllicationTests.java
@@ -57,4 +57,10 @@ class CashCardApllicationTests {
         assertThat(id).isNotNull();
         assertThat(amount).isEqualTo(250.00);
     }
+
+    @Test
+    void shouldReturnAllCashCardsWhenListIsRequested(){
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
