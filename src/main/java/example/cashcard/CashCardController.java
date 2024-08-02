2ee125af9e3c1bf064ff42cf01fedcdd2789c5c6
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-02 10:04:08 +0300

    [FIX] post endpoint now accepts request body and returns 201

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Optional;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -28,7 +31,7 @@ class CashCardController {
     }
 
     @PostMapping
-    private ResponseEntity<Void> createCashCard(){
-        return null;
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest){
+        return ResponseEntity.created(URI.create("/what/should/go/here?")).build();
     }
 }
