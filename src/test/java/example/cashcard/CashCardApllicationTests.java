a9ca84836c646a3df3697f2a0d3e0c5d4e9ed59b
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-09 09:59:19 +0300

    [TEST] testing trying to delete cash card that owned by other user

--- src/test/java/example/cashcard/CashCardApllicationTests.java
+++ src/test/java/example/cashcard/CashCardApllicationTests.java
@@ -234,4 +234,12 @@ class CashCardApllicationTests {
                 .exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
         assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
+
+    @Test
+    void shouldNotAllowDeletionOfCashCardsTheyDoNotOwn() {
+        ResponseEntity<Void> deleteResponse = restTemplate
+                .withBasicAuth("sarah1", "abc123")
+                .exchange("/cashcards/102", HttpMethod.DELETE, null, Void.class);
+        assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
 }
