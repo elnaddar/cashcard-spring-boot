3ae551d7e2953371b5714633ae940e8b3e8a2645
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-09 09:45:25 +0300

    [TEST] updated DELETE testcase to ensure that record is readlly deleted.

--- src/test/java/example/cashcard/CashCardApllicationTests.java
+++ src/test/java/example/cashcard/CashCardApllicationTests.java
@@ -220,6 +220,11 @@ class CashCardApllicationTests {
                 .withBasicAuth("sarah1", "abc123")
                 .exchange("/cashcards/99", HttpMethod.DELETE, null, Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        ResponseEntity<String> getResponse = restTemplate
+                .withBasicAuth("sarah1", "abc123")
+                .getForEntity("/cashcards/99", String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
 }
