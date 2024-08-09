d22562deb69f68b14172f64ee75ec6c05052fcb2
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-08-09 20:24:55 +0300

    [FEATURE] added swagger

---
+++ src/main/java/example/cashcard/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package example.cashcard;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CashCard Spring Boot Application")
+                        .version("1.1")
+                        .description(
+                                "This is a Spring Boot application for managing CashCards. The application exposes a RESTful API for creating, retrieving, updating, and deleting CashCards. It also includes a basic security configuration."))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("basicAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("basic")));
+    }
+}
--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -14,37 +14,43 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 class SecurityConfig {
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request
-                .requestMatchers("/cashcards/**")
-                .hasRole("CARD-OWNER"))
-                .httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable());
-        return http.build();
-    }
+        @Bean
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http.authorizeHttpRequests(request -> request
+                                .requestMatchers("/cashcards/**")
+                                .hasRole("CARD-OWNER"))
+                                .httpBasic(Customizer.withDefaults())
+                                .csrf(csrf -> csrf.disable());
 
-    @Bean
-    UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
-        User.UserBuilder users = User.builder();
-        UserDetails sarah = users.username("sarah1")
-                .password(passwordEncoder.encode("abc123"))
-                .roles("CARD-OWNER")
-                .build();
-        UserDetails hankOwnsNoCards = users.username("hank-owns-no-card")
-                .password(passwordEncoder.encode("123abc"))
-                .roles("NON-OWNER")
-                .build();
-        UserDetails kumar = users
-                .username("kumar2")
-                .password(passwordEncoder.encode("xyz789"))
-                .roles("CARD-OWNER")
-                .build();
-        return new InMemoryUserDetailsManager(sarah, hankOwnsNoCards, kumar);
-    }
+                http.authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .anyRequest().authenticated())
+                                .httpBasic(Customizer.withDefaults());
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+                return http.build();
+        }
+
+        @Bean
+        UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+                User.UserBuilder users = User.builder();
+                UserDetails sarah = users.username("sarah1")
+                                .password(passwordEncoder.encode("abc123"))
+                                .roles("CARD-OWNER")
+                                .build();
+                UserDetails hankOwnsNoCards = users.username("hank-owns-no-card")
+                                .password(passwordEncoder.encode("123abc"))
+                                .roles("NON-OWNER")
+                                .build();
+                UserDetails kumar = users
+                                .username("kumar2")
+                                .password(passwordEncoder.encode("xyz789"))
+                                .roles("CARD-OWNER")
+                                .build();
+                return new InMemoryUserDetailsManager(sarah, hankOwnsNoCards, kumar);
+        }
+
+        @Bean
+        PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 }
